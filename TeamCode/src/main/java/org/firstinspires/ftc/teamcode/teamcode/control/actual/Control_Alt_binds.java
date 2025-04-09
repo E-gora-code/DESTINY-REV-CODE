6802b6dae82b16f149f5d996a25b331023e3ec13
Egor <e-gora.egor@yandex.ru>
2025-04-10 00:35:15 +0700

    return stabilization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/control/actual/Control_Alt_binds.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/control/actual/Control_Alt_binds.java
@@ -40,7 +40,7 @@ public class Control_Alt_binds extends LinearOpMode {
     //end
     // Reset
     ElapsedTime presed_reset_ang_timer = new ElapsedTime();
-    boolean is_reseted_ever = false;
+    boolean is_reseted_ever = true;
     //end
 
 
