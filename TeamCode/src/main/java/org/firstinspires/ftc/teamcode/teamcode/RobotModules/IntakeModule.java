a838cb60c0ea1741e4ec3232bea1f25595880023
Egora <e-gora.egor@yandex.ru>
2025-11-09 13:52:00 +0700

    Abstract module classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/RobotModules/IntakeModule.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.teamcode.RobotModules;
+
+public  class IntakeModule {
+    public void setPower(double power){
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/RobotModules/TurretModule.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.teamcode.RobotModules;
+
+public class TurretModule {
+    public void setWheelSpeed(double speed){
+
+    }
+    public void rotate_to(double rotation, double angle){
+
+    }
+    public void init_all(){
+
+    }
+    public void class_tick(){
+
+    }
+}
